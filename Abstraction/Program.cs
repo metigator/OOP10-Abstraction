1688a9c5d887095bc87cae4c93e1d9a119f4a362
Metigator <87314838+metigator@users.noreply.github.com>
2023-05-28 14:38:24 -0400

    initial git

---
+++ Abstraction/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Abstraction
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BankAccount account1 = new Savingccount();
+            account1.AccountNumber = "1111";
+            account1.Balance = 6000;
+
+            account1.Deposit(100);
+            account1.Withdraw(1000);
+
+        }
+
+    }
+
+    public abstract class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+
+        public abstract void Deposit(decimal amount);
+
+        public abstract void Withdraw(decimal amount);
+    }
+
+    public class CheckingAccount : BankAccount
+    {
+        private const decimal DailyWithdrawalLimit = 5000;
+        public override void Deposit(decimal amount)
+        {
+            //implementation details
+            if (amount < 0)
+                throw new InvalidOperationException();
+
+            Balance += amount;
+        }
+        public override void Withdraw(decimal amount)
+        {
+            //implementation details 
+
+            if (amount < 0)
+                throw new InvalidOperationException();
+
+            if (amount > DailyWithdrawalLimit)
+                throw new InvalidOperationException();
+
+            Balance -= amount;
+        }
+    }
+
+    public class Savingccount : BankAccount
+    {
+        private const decimal DailyWithdrawalLimit = 2000;
+        public override void Deposit(decimal amount)
+        {
+            //implementation details
+            if (amount < 0)
+                throw new InvalidOperationException();
+
+            Balance += amount;
+        }
+        public override void Withdraw(decimal amount)
+        {
+            //implementation details
+
+            if (amount < 0)
+                throw new InvalidOperationException();
+
+            if (amount > DailyWithdrawalLimit)
+                throw new InvalidOperationException();
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ AbstractionWithInterface/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace AbstractionWithInterface
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BankAccount account1 = new Savingccount();
+            account1.AccountNumber = "1111";
+            account1.Balance = 6000;
+
+            account1.Deposit(100);
+            account1.Withdraw(1000);
+
+        }
+
+    }
+
+    public interface IBankAccount
+    {
+        string AccountNumber { get; set; }
+        decimal Balance { get; set; }
+        void Deposit(decimal amount);
+        void Withdraw(decimal amount);
+    }
+
+    public class CheckingAccount : IBankAccount
+    {
+        private const decimal DailyWithdrawalLimit = 5000;
+
+        public string AccountNumber { get; set; }
+        public decimal Balance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Deposit(decimal amount)
+        {
+            //implementation details
+            if (amount < 0)
+                throw new InvalidOperationException();
+
+            Balance += amount;
+        }
+        public override void IBankAccount(decimal amount)
+        {
+            //implementation details 
+
+            if (amount < 0)
+                throw new InvalidOperationException();
+
+            if (amount > DailyWithdrawalLimit)
+                throw new InvalidOperationException();
+
+            Balance -= amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Savingccount : BankAccount
+    {
+        private const decimal DailyWithdrawalLimit = 2000;
+        public override void Deposit(decimal amount)
+        {
+            //implementation details
+            if (amount < 0)
+                throw new InvalidOperationException();
+
+            Balance += amount;
+        }
+        public override void Withdraw(decimal amount)
+        {
+            //implementation details
+
+            if (amount < 0)
+                throw new InvalidOperationException();
+
+            if (amount > DailyWithdrawalLimit)
+                throw new InvalidOperationException();
+
+            Balance -= amount;
+        }
+    }
+}
